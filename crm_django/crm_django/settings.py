4b1624d7d118f7ea576053d3d774ee23466786de
pikkapika <naveenrawatGT@gmail.com>
2021-04-27 00:54:05 +0530

    removed secret key from settings.py file

--- crm_django/crm_django/settings.py
+++ crm_django/crm_django/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4o#e0r0s0%!^d75_e!w@+pih9v)j4afn%-1_0!x0(qms$n=hsf'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
