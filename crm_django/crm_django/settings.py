7ee8e44018c06803267ee45ad0b2509e21fe3f23
naveenrawatgt <46143389+naveenrawatgt@users.noreply.github.com>
2021-05-09 14:41:06 +0530

    Production setting added fixes

--- crm_django/crm_django/settings.py
+++ crm_django/crm_django/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -178,3 +178,7 @@ if not DEBUG:
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
     X_FRAME_OPTIONS = "DENY"
+
+    ALLOWED_HOSTS = [
+        "*"
+    ]
